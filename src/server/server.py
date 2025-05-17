f7d83519d80579beec8c26eed2be46b8337015a7
Tarcisio Reis <tarcisiofi@hotmail.com>
2025-05-17 15:52:58 -0300

    feat: Implementa sistema de compartilhamento de clipboard em rede
    servidor funcionando

--- src/server/server.py
+++ src/server/server.py
@@ -1,9 +1,9 @@
-from server.app import run_server
+from src.server.app import run_server
 from src.client.clipboard_monitor import ClipboardMonitor
 
 #iniciar servidor
 
-server_url = "http://localhost:5000"
+server_url = "http://192.168.254.224:5000"
 monitor = ClipboardMonitor(server_url)
 monitor.start()
 
